df0f33f060ad6ba302eff925a2446158205e74da
Ashmit <ashpx3293@gmail.com>
2025-12-27 15:49:45 +0530

    signatures allowed in main method

---
+++ eclipse-workspace/PCE/src/com/commandline/commandLineArgument.java
@@ -0,0 +1,11 @@
+package com.commandline;
+
+public class commandLineArgument {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(args[0]+" ");
+		System.out.println(args[1]+" ");
+	}
+
+}
